abc210093e2ee5558058dc2e76bffc1bd21ab316
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-24 19:52:40 -0500

    05-some-every.test.js

--- index.js
+++ index.js
@@ -270,21 +270,21 @@ function getBiggestBoxOfficeMovie(movies) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function allSongsAreOverTwoMinutes(songs) {}
+const allSongsAreOverTwoMinutes = (songs) => songs.every(song => song.runtimeInSeconds > 120)
 
 /**
  * Returns `true` if any song is over four minutes. Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsOverFourMinutes(songs) {}
+const anySongIsOverFourMinutes = (songs) => songs.some(song => song.runtimeInSeconds > 240)
 
 /**
  * Returns `true` if any song is by the artist "Peanut". Otherwise, return `false`.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {boolean}
  */
-function anySongIsByPeanut(songs) {}
+const anySongIsByPeanut = (songs) => songs.some(song => song.artist === 'Peanut')
 
 // Do not change anything below this line.
 module.exports = {
